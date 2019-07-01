f166c072adbf9fb583d0ebc9c8e2a44ceaff638e
adonayab <adonayab15@hotmail.com>
2019-07-01 18:15:27 -0500

    Added some more tests

--- Test/CaesarEncryptTest.java
+++ Test/CaesarEncryptTest.java
@@ -11,16 +11,28 @@ public class CaesarEncryptTest {
         CaesarEncrypt unCat = new CaesarEncrypt("png", 13);
         CaesarEncrypt capCat = new CaesarEncrypt("Cat", 13);
         CaesarEncrypt charsCat = new CaesarEncrypt("!?ca./t", 13);
+        CaesarEncrypt rot2Cat = new CaesarEncrypt("cat", 2);
+        CaesarEncrypt rot3Cat = new CaesarEncrypt("!?ca./t", 3);
+        CaesarEncrypt longCat = new CaesarEncrypt("cats are amazing", 5);
+
 
         String encrypted = cat.encrypt();
         String encrypted2 = unCat.encrypt();
         String encrypted3 = capCat.encrypt();
         String encrypted4 = charsCat.encrypt();
+        String encrypted5 = rot2Cat.encrypt();
+        String encrypted6 = rot3Cat.encrypt();
+        String encrypted7 = longCat.encrypt();
 
         assertEquals("png",encrypted);
         assertEquals("cat",encrypted2);
         assertEquals("Png",encrypted3);
         assertEquals("!?pn./g",encrypted4);
+        assertEquals("ecv",encrypted5);
+        assertEquals("!?fd./w",encrypted6);
+        assertEquals("hfyx fwj frfensl",encrypted7);
+
+
     }
 
 }
